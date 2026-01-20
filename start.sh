190161b2041096896e3c0051fad059c4922ce2e0
bestqrov <abltop44@gmail.com>
2026-01-20 15:42:53 +0100

    Fix port configuration: Use BACKEND_PORT for backend, let platform set PORT for frontend

--- start.sh
+++ start.sh
@@ -1,7 +1,7 @@
 #!/bin/sh
 
 # Start backend in background
-npm run start:prod &
+BACKEND_PORT=3001 npm run start:prod &
 
 # Start frontend
-cd frontend && PORT=3000 NEXT_PUBLIC_API_URL=http://localhost:3001 npm start
+cd frontend && NEXT_PUBLIC_API_URL=http://localhost:3001 npm start
